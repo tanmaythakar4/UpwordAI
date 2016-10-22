b3c93e2bc8da7719bb08ef297b8966cddd9b5f64
tanmaythakar4 <tanmaythakar4@gmail.com>
2016-10-22 14:09:12 -0400

    initial project version

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+''' 
+This python is our starting point from where 
+we haldle the game form starting to ending
+like calling server to start get hashnumber and save globally
+or calling nextmove.
+
+we can say basicaly it's our frontend for project
+
+'''
+
+from Network import connection
