4b9e3c5958d2e803090174d95586b67c666f026c
Parth Bhatt <pbjm2493@gmail.com>
2016-10-23 12:53:43 -0400

    Comments changed in bag.py and Upwords_Rules file is added.

--- Board/bag.py
+++ Board/bag.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 '''
-This file maintain the bag of character or tile which we are using through out 
-the game
-for example it may contain method
-to add letter in starting,
-shuffule the bag,
-grab,
-putback,
-check(empty or not)
+Initial distribution of tiles(letter)
+This file maintain the bag of all characters or tiles which we are using through out the game.
 
+Methods: 	--> Explanation
+
+add() 		--> Initially add all tiles to the bag.
+shuffle() 	--> Shuffule the bag.
+grab()		--> Grab the tiles from the bag randomly.
+isEmpty()	--> Check whether bag is empty or not.
+putBack()	--> Swapping of tiles from the tray and bag. But if anybody uses swap then it will count 
+				as one move. 
 '''
+
+class Bag:
+	
