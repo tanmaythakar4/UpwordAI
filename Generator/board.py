6346d3df3eaee29bfe8f606a3a07c098f92682b5
tanmaythakar4 <tanmaythakar4@gmail.com>
2016-11-07 16:01:41 -0500

    Some Changes in Board and dictionary .......

--- Generator/board.py
+++ Generator/board.py
@@ -10,3 +10,236 @@ validate(word,score,timing)
 check upwordrules
 
 '''
+import dictionary
+class Board:
+    
+    Debug_errors = True
+    DICTIONARY_FILE = '../dictionary.txt'
+    BOARD_SIZE = 10
+    START_POSITION = [(4,4),(4,5),(5,4),(5,5)]
+                      
+    def __init__(self):
+        self.tiles = []
+        for x in range(Board.BOARD_SIZE):
+            self.tiles.append([])
+            for y in range(Board.BOARD_SIZE):
+                #here we are adding tile and some status but as of now we dont know 
+                #which status will helpfull so it's tile only
+                self.tiles[x].append((None))
+        
+        # Load the dictionary        
+        self.dictionaryf = dictionary.Dictionary(Board.DICTIONARY_FILE)
+        
+        # load dictionary frequency usage (Working)
+        #self.wordfreq = wordFrequency.wordFrequency()
+        
+        #self.resetBoard()
+    
+        
+    '''
+    now will proceed like first we will generate 
+    (a) the tentative moves from executemove function() 
+    (b) then we call play function which find the valid move (checking all the rules) return (tiles,point)
+    (c) add that tile into board and add the point 
+    and finally send this board to server , tiles[][]    
+    
+    
+        
+    '''
+    '''
+    (a) calculate the best move and place the tile on board and call play function (b)
+    
+    
+    This is our basic algorithm where we start with the brute force .because if its not take
+    longer time then this algorithm give high scoring move,
+    Steps for (a)
+    
+    (1) First create a list of seed if its first turn then add START_POSITION or 
+        else add all the blank and adjecent to the unoccupied tile into seed.
+    (2)    
+    
+    '''
+    
+    def executeMove(self,isFirstMove):
+
+        
+        # calculate the time still working
+        
+        
+        #(1)
+        seeds = []
+
+        if isFirstMove:
+                seeds = START_POSITION
+        else:
+            for x in range(self.BOARD_SIZE):
+                for y in range(self.BOARD_SIZE):
+                    if self.tiles[x][y] != None:
+                        
+                        # go up and add tile till end
+                        if y>0 and self.tiles[x][y-1] == None:
+                            if (x,y-1) not in seeds:
+                                seeds.append((x,y-1))
+                                
+                        # go down and add tile till end
+                        if y< self.BOARD_SIZE - 1 and self.tiles[x][y+1] == None:
+                            if (x,y+1) not in seeds:
+                                seeds.append((x,y+1))
+                    
+                                
+                        # go left and add tile till end
+                        if x>0 and self.tiles[x-1][y] == None:
+                            if (x-1,y) not in seeds:
+                                seeds.append((x-1,y))
+                                
+                                
+                        # go right and add tile till end
+                        if x<self.BOARD_SIZE - 1 and self.tiles[x+1][y] == None:
+                            if (x+1,y) not in seeds:
+                                seeds.append((x+1,y))
+                                
+                        
+        print(seeds)
+            
+        (maxpoint,maxtile) = -1000,None
+        
+        tileSlots = []
+        # Step 2: add all the horizontal and verticle tiles where we can put the word
+        #initiate tray (Remaining)
+        for (x,y) in seeds:
+            for lo in range(0,len(self.tray)):
+                for hi in range(0,len(self.tray)-lo):
+                    
+                    #Horizontal tile slot
+                     horz = [((x,y),self.tiles[x][y])]
+                     loCount = 0
+                     hiCount = 0
+                     
+                     #left
+                     xPos,yPos = x-1,y
+                     while xPos>0 and (loCount<lo or self.tiles[xPos][yPos] != None):
+                         loCount += 1
+                         horz.insert(0,((xPos,yPos),self.tiles[xPos][yPos]))
+                         xPos -=1
+                         
+                     #RIGHT
+                     xPos,yPos = x+1,y
+                     while xPos<self.BOARD_SIZE-1 and (hiCount<hi or self.tiles[xPos][yPos] != None):
+                         hiCount += 1
+                         horz.append(((xPos,yPos),self.tiles[xPos][yPos]))
+                         xPos +=1
+                    
+                        
+                    #verticle tile slot
+                     vert = [((x,y),self.tiles[x][y])]
+                     loCount = 0
+                     hiCount = 0
+                     
+                     #up
+                     xPos,yPos = x,y-1
+                     while yPos>0 and (loCount<lo or self.tiles[xPos][yPos] != None):
+                         loCount += 1
+                         vert.insert(0,((xPos,yPos),self.tiles[xPos][yPos]))
+                         yPos -=1
+                         
+                     #down
+                     xPos,yPos = x,y+1
+                     while yPos<self.BOARD_SIZE-1 and (hiCount<hi or self.tiles[xPos][yPos] != None):
+                         hiCount += 1
+                         vert.append(((xPos,yPos),self.tiles[xPos][yPos]))
+                         yPos +=1
+                            
+                             
+                     tileSlots.append(horz)
+                     tileSlots.append(vert)
+       # print(tileSlots)
+                     #print("horz",horz)
+                     #print("vert",vert)
+                    
+                    
+        #remove all the duplicates
+        tileslotsmap = {}
+        i = 0
+        orignalsize = len(tileSlots)
+        numEliminate = 0
+        while i < len(tileSlots):
+            slot = tileSlots[i]
+            (x1,y1) = slot[0]
+            (x2,y2) = slot[-1]
+            
+            if tileslotsmap.get((x1,y1,x2,y2),False):
+                            tileSlots.pop(i)
+                            numEliminate += 1
+            else:
+                tileslotsmap[x1,y1,x2,y2] = True
+                i += 1
+        print(tileSlots)
+                            
+        for tileslot in tileSlots:
+            emptySlots = []
+            wordBuilt = []
+            slotPosition= {}
+            for (x,y) , tile in tileslot:
+                slotPosition[(x,y)] = True
+                if tile == None:
+                    emptySlots.append((x,y))
+                wordBuilt.append(tile)
+            print(emptySlots)
+            print(wordBuilt)
+            print(slotPosition)
+            
+            (point, tiles, blankes) = self.tryallPermutation(isFirstMove,wordBuilt,emptySlots,self.tray) 
+             # NOw we have best tile so play them 
+             #
+             #TO BE CONTINUE
+        '''
+        Here we have given a set of position ,this function will try all the combination of tray tiles 
+        that could be inserted and return the score and tile placed of the highrest scoring combinayion bybasking the board
+        
+        
+        '''
+    def tryallPermutation(self,isFirstMove, word, slots, traytiles, tilesPlaced = []):
+        
+        if len(slots) == 0:
+            
+            blankAssignment = []
+            seedRation = (-1, -1)
+            
+            # find a word before a validation
+            i=0
+            spelling= ""
+            for tile in word:
+                if tile != None:
+                    spelling += tile.letter
+                    
+                else:
+                    #spelling += tilesplaced[i][1].letter
+                    spelling += tilesplaced[i]
+                    i+=1
+                    
+            # if there no blank
+            if not ' ' in spelling:
+                if self.dictionaryf.isValid(spelling) or len(slots) == 1:
+                    
+                    
+                
+                
+                    
+        
+            
+                    
+                    
+                    
+ #TEST		
+if __name__ == '__main__':	
+  boards = Board()
+  #Testing
+  boards.BOARD_SIZE = 5
+  boards.tiles = [[None,None,None,None,None],[None,None,'G',None,None],[None,None,'O',None,None],
+                      [None,None,'A',None,None],[None,None,'L',None,None]]
+
+                      
+  boards.tray = ['B','C','D','E','F','G','H',]
+        
+  boards.executeMove(False)    
+                    
--- Generator/dictionary.py
+++ Generator/dictionary.py
@@ -9,4 +9,125 @@ validate word
 setcount(wordcount part of statstic)
 sequence of words
 
+here I asume that before we start to run this we make another textfile from dictionary which contain 
+ word and usage which we are going to maintain
+ Question :
+     Here worsmith have that textfile which contain count from last 1600 game so it's look good . can we use that file ?
+     Or
+    at inital stage we need to initialize with 0.
+    
 '''
+
+import board,time
+class Dictionary:
+    
+    def __init__(self,filename):
+        
+        '''
+        list of words from text file
+        '''
+        self.words = {}
+        self.lookuptime = 0
+        dictionaryfile = open(filename,'r')
+        for line in dictionaryfile:
+            line = line.rstrip();
+            tokens = line.split();
+            if len(tokens) == 1:
+                self.words[tokens[0]] = 1;
+            else:
+                self.words[tokens[0]] = int(tokens[1]) 
+            
+            
+    
+    def findlength(self):
+        return self.words.__len__()
+        
+    def isValid(self,word):
+        print(word)
+        #print(word in self.words)
+        # if you want to find out the time to find it's valid or not . it's for if it takes longer you can stop
+        
+        if board.Board.Debug_errors:
+            start = time.time()
+        if word in self.words:
+            sucsess = True
+        else:
+            sucsess = False
+            
+        if board.Board.Debug_errors:
+            timespent = time.time() - start
+            self.lookuptime += timespent
+        return sucsess
+        
+    # here we replace the blank with the word because they append the blanks to the adjecent blank from board
+    
+    def matchWithBlanks(self,word,assignment=[]):
+        
+        if not ' ' in word:
+            if self.isValid(word):
+                return [assignment]
+            else:
+                return []
+        else:
+            i = word.find(' ')
+            blanAssignment = []
+            for num in range(ord('A'),ord('Z')+1):
+                char = chr(num)
+                if i==0:
+                    newword = char + word[1:]
+                elif i == len(word)-1:
+                    newword = word[i:-1] + char
+                else:
+                    newword = word[:i] + char + word[i+1:]
+            newAssignment = assignment[:]
+            newAssignment.append(char)
+            results = self.matchWithBlanks(newword,newAssignment)
+            for result in results:
+                blanAssignment.append(result)
+                
+            return blanAssignment
+            
+    # set the word count in dictionary for future use
+    
+    def setUsage(self,word,usage):
+        word = word.upper().rstrip()
+        if self.isValid(word):
+            self.words[word] = usage
+            return True
+        else:
+            return False
+            
+    # Save the dictionary in file
+    
+    def savDictionory(self,fileName):
+        with open(fileName,'W') as output:
+            keyList = self.words.keys()
+            keyList.sort()
+            for w in keyList:
+                output.write(W+"\t"+str(words[w]+"\n"))
+                
+                
+    # reset the looktime we manage for game
+    def resetLookup(self):
+         self.lookuptime = 0
+         
+                
+    
+
+            
+#TEST
+if __name__ == '__main__':
+    dic = Dictionary('../dictionary.txt')
+    print(dic.findlength())
+    d = "NAUTICAL"
+    
+    #print(dic.isValid(d))
+    #print(dic.lookuptime)
+     
+    e = "HOME"
+    #print(dic.isValid(e))
+   # print(dic.lookuptime)
+    
+    
+    print(dic.matchWithBlanks('H'+' '+'M'+'E'))
+    
