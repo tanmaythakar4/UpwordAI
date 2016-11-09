2d97a3b6de9e8ce3bef6298b68040723d1a0b683
tanmaythakar4 <tanmaythakar4@gmail.com>
2016-11-09 17:03:28 -0500

    commit updated

---
+++ BoardD/rack.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+'''
+this file contain a method to gerate the pil or rack of 7 character which we are using 
+through out the game to find out the word
+like add,shuffle,regenerate,empty or not,
+ etc.
+ 
+ Testing
+ '''
